997a935a5e1cee11d05543ea48af6a39d0633eca
jhj2075 <flg989@naver.com>
2025-01-22 22:31:46 +0900

    01.22

---
+++ src/P_가장큰수/Solution.java
@@ -0,0 +1,21 @@
+package P_가장큰수;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] answer = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++){
+            answer[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(answer, (o1,o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (answer[0].equals("0")){
+            return "0";
+        }
+
+        return String.join("", answer);
+    }
+}
