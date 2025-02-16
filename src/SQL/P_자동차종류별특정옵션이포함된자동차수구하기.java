825f41f2836296842c57ddf0618381102112869a
jhj2075 <flg989@naver.com>
2025-02-17 00:21:51 +0900

    02.16

---
+++ src/SQL/P_자동차종류별특정옵션이포함된자동차수구하기.java
@@ -0,0 +1,9 @@
+package SQL;
+
+public class P_자동차종류별특정옵션이포함된자동차수구하기 {
+//    SELECT CAR_TYPE, count(*) as CARS
+//    FROM CAR_RENTAL_COMPANY_CAR
+//    WHERE OPTIONS LIKE '%통풍시트%' or OPTIONS LIKE '%열선시트%' or OPTIONS LIKE '%가죽시트%'
+//    GROUP BY CAR_TYPE
+//    ORDER BY CAR_TYPE
+}
