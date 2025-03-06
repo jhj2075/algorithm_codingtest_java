32e37db3cb752a0f7a9d5814b6f7d04ac6e57bea
jhj2075 <flg989@naver.com>
2025-03-06 09:50:10 +0900

    03.06

---
+++ src/Baekjoon/Main_2745.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main_2745 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] n = sc.next().toCharArray();
+        int b = sc.nextInt();
+
+        long sum = 0;
+        for (int i = 0; i < n.length; i++) {
+            if ((int)n[i] >= 65 && (int)n[i] <= 90)
+                sum += ((long)n[i] - 55) * (long)Math.pow(b, n.length - i - 1);
+            else
+                sum += Long.parseLong(String.valueOf(n[i])) * (long)Math.pow(b, n.length - i - 1);
+        }
+        System.out.println(sum);
+    }
+}
