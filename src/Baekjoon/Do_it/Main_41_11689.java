52999ccd6f6d186d5352602ff7306b6ae91e6c08
jhj2075 <flg989@naver.com>
2025-03-10 23:16:30 +0900

    03.10

--- src/Baekjoon/Do_it/Main_41_11689.java
+++ src/Baekjoon/Do_it/Main_41_11689.java
@@ -1,17 +1,19 @@
 package Baekjoon.Do_it;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 // 정수론 - 오일러 피 함수 구현하기
 public class Main_41_11689 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
         long ans = n;
-        for (long i = 2; i < Math.sqrt(n); i++) {
+        for (long i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0){
-                ans = ans - (ans / i);
+                ans = ans - ans / i;
                 while (n % i == 0){
                     n /= i;
                 }
