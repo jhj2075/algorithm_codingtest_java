771572ea2656985f177817faf1ace4dd967c4237
jhj2075 <flg989@naver.com>
2025-03-11 23:08:48 +0900

    03.11

---
+++ src/Baekjoon/Do_it/Main.java
@@ -0,0 +1,26 @@
+package Baekjoon.Do_it;
+
+import java.util.Scanner;
+
+
+// 정수론 - 최소 공배수 구하기
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int ans = a * b / gcd(a, b);
+            System.out.println(ans);
+        }
+    }
+    public static int gcd(int a, int b){
+        if (b == 0){
+            return a;
+        }
+        else {
+            return gcd(b, a % b);
+        }
+    }
+}
