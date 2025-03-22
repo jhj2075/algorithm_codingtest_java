65a1dddb7d16b8c8b5013d03342e933f4e71cbc5
jhj2075 <flg989@naver.com>
2025-03-22 23:45:34 +0900

    refactor

--- src/Baekjoon/Do_it/Main_35_1931.java
+++ src/Baekjoon/Do_it/Main_35_1931.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
+// 그리디 - 회의실 배정
 public class Main_35_1931 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
