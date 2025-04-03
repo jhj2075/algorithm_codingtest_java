2dbd6e026e812018cd17e69daf86c86edf16bc50
jhj2075 <flg989@naver.com>
2025-04-04 00:35:38 +0900

    04.03

--- src/Baekjoon/Do_it/Main_68_1068.java
+++ src/Baekjoon/Do_it/Main_68_1068.java
@@ -1,20 +1,58 @@
 package Baekjoon.Do_it;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+
+// 트리 - 리프 노드의 개수 구하기
 public class Main_68_1068 {
+    static ArrayList<Integer>[] tree;
+    static boolean[] visited;
+    static int leafCnt;
+    static int delete;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        ArrayList<Integer>[] tree = new ArrayList[n];
+        tree = new ArrayList[n];
+        visited = new boolean[n];
+        leafCnt = 0;
         for (int i = 0; i < n; i++) {
             tree[i] = new ArrayList<>();
         }
+        int root = 0;
         for (int i = 0; i < n; i++) {
             int parent = sc.nextInt();
-            if (parent != -1)
+            if (parent != -1) {
                 tree[parent].add(i);
+            }
+            else root = i;
+        }
+        delete = sc.nextInt();
+        BFS(root);
+        System.out.println(leafCnt);
+    }
+    static void BFS(int n){
+        if (n == delete) return;
+        visited[n] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        while (!queue.isEmpty()){
+            int now = queue.poll();
+
+            int childCnt = 0;
+            for (int i : tree[now]) {
+                if (i == delete) continue;
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                    childCnt++;
+                }
+            }
+            if (childCnt == 0){
+                leafCnt++;
+            }
         }
     }
 }
