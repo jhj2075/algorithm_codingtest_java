699cd2703ea10074c07066533348bd8cb9f9f7c3
jhj2075 <flg989@naver.com>
2025-04-10 00:16:24 +0900

    04.10

--- src/Baekjoon/Do_it/Main_74_11437.java
+++ src/Baekjoon/Do_it/Main_74_11437.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+
+// 트리 - 최소 공통 조상 구하기 1
 public class Main_74_11437 {
     static boolean[] visited;
     static ArrayList<Integer>[] arrayLists;
@@ -34,36 +36,35 @@ public class Main_74_11437 {
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            int aDepth = arr[1][a];
-            int bDepth = arr[1][b];
-
-            int bigDepth = Math.max(aDepth, bDepth);
-            int smallDepth = Math.min(aDepth, bDepth);
             // 두 노드 중 깊이가 더 깊은 노드부터 한 칸씩 위로 올라가기
-            while (bigDepth > smallDepth){
-
+            while (arr[1][a] > arr[1][b]) {
+                a = arr[0][a];
             }
-
-
+            while (arr[1][b] > arr[1][a]) {
+                b = arr[0][b];
+            }
+            // 두 노드의 깊이가 같아지면 두 노드가 같아질 때까지 같이 한 칸씩 위로 올라가기
+            while (a != b) {
+                a = arr[0][a];
+                b = arr[0][b];
+            }
+            System.out.println(a);
         }
-        // 두 노드의 깊이가 같아지면 두 노드가 같아질 때까지 같이 한 칸씩 위로 올라가기
     }
     static void BFS(int n){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(n);
         visited[n] = true;
-        int depth = 1;
         while (!queue.isEmpty()){
             int now = queue.poll();
             for (int i : arrayLists[now]){
                 if (!visited[i]) {
                     visited[i] = true;
                     arr[0][i] = now;
-                    arr[1][i] = depth;
+                    arr[1][i] = arr[1][now] + 1;
                     queue.add(i);
                 }
             }
-            depth++;
         }
     }
 }
