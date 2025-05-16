1182f21306594c0a6584feee324486c387820b44
jhj2075 <flg989@naver.com>
2025-05-16 21:45:43 +0900

    05.16

--- src/Baekjoon/Do_it/Main_69_14425.java
+++ src/Baekjoon/Do_it/Main_69_14425.java
@@ -1,50 +1,56 @@
 package Baekjoon.Do_it;
 
-import java.util.HashSet;
 import java.util.Scanner;
 
 
-// 트리 - 문자열 찾기 (트라이 문제인데, 그냥 hashset으로 품)
+// 트리 - 문자열 집합 (트라이로 품)
 public class Main_69_14425 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        tNode root = new tNode();
 
-        HashSet<String> hashSet = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            hashSet.add(sc.next());
-        }
-        int cnt = 0;
-        for (int i = 0; i < m; i++) {
-            if (hashSet.contains(sc.next())){
-                cnt++;
+        // 트라이 생성
+        while (n > 0){
+            String text = sc.next();
+            tNode now = root;
+
+            for (int i = 0; i < text.length(); i++) {
+                char c = text.charAt(i);
+                if (now.next[c - 'a'] == null){
+                    now.next[c - 'a'] = new tNode(); // 노드를 채우기만 함으로써 그 알파벳이 있다는 걸 알림
+                }
+                now = now.next[c - 'a'];
+
+                if (i == text.length() - 1){
+                    now.isEnd = true;
+                }
             }
+            n--;
+        }
+
+        int cnt = 0;
+        // 트라이 탐색
+        while (m > 0){
+            String text = sc.next();
+            tNode now = root;
+
+            for (int i = 0; i < text.length(); i++) {
+                char c = text.charAt(i);
+                if (now.next[c - 'a'] == null){
+                    break;
+                }
+                now = now.next[c - 'a'];
+                if (i == text.length() - 1 && now.isEnd)
+                    cnt++;
+            }
+            m--;
         }
         System.out.println(cnt);
     }
-    // 이렇게 풀면 시간초과
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int m = sc.nextInt();
-//        String[] S = new String[n];
-//        for (int i = 0; i < n; i++) {
-//            S[i] = sc.next();
-//        }
-//        String[] M = new String[m];
-//        for (int i = 0; i < m; i++) {
-//            M[i] = sc.next();
-//        }
-//        int cnt = 0;
-//        for (int i = 0; i < m; i++) {
-//            for (int j = 0; j < n; j++) {
-//                if (M[i].equals(S[j])){
-//                    cnt++;
-//                    break;
-//                }
-//            }
-//        }
-//        System.out.println(cnt);
-//    }
+}
+class tNode{
+    tNode[] next = new tNode[26];
+    boolean isEnd;
 }
