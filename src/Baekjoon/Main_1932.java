8625543638312bae9afb98f1b91144188b6d4585
jhj2075 <flg989@naver.com>
2026-01-04 23:11:00 +0900

    01.04

--- src/Baekjoon/Main_1932.java
+++ src/Baekjoon/Main_1932.java
@@ -1,4 +1,35 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
+
+// DP - 정수 삼각형
 public class Main_1932 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] dp = new int[n+1][n+1];
+        dp[1][1] = arr[1][1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i+1][j] = Math.max(dp[i][j] + arr[i+1][j], dp[i+1][j]);
+                dp[i+1][j+1] = Math.max(dp[i][j] + arr[i+1][j+1], dp[i+1][j+1]);
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, dp[n][i]);
+        }
+
+        System.out.println(max);
+    }
 }
--- src/Baekjoon/Main_9095.java
+++ src/Baekjoon/Main_9095.java
@@ -1,4 +1,28 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
+
+// DP - 1, 2, 3 더하기
 public class Main_9095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        // 4 이상일 때의 경우의 수 = (전거에서 + 1 한 경우) + (전전거에서 + 2 한 경우) + (전전전거에서 + 3 한 경우)
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+
+            System.out.println(dp[n]);
+        }
+    }
 }
