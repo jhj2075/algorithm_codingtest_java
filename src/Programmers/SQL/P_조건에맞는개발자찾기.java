07168bd510b72687548484c5d1c2b8f57a49bee5
jhj2075 <flg989@naver.com>
2026-02-17 20:59:17 +0900

    02.17

---
+++ src/Programmers/SQL/P_조건에맞는개발자찾기.java
@@ -0,0 +1,10 @@
+package Programmers.SQL;
+
+public class P_조건에맞는개발자찾기 {
+//    select distinct ID, EMAIL, FIRST_NAME, LAST_NAME
+//    from DEVELOPERS, SKILLCODES
+//    where SKILL_CODE & (select sum(CODE)
+//    from SKILLCODES
+//    where NAME in ("Python", "C#"))
+//    order by ID ASC
+}
