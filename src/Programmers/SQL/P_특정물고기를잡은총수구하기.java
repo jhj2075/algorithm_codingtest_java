9ec157953abf1e979ad6b3cdfe8a2f30e3c023dc
jhj2075 <flg989@naver.com>
2026-04-27 19:55:50 +0900

    04.27

---
+++ src/Programmers/SQL/P_특정물고기를잡은총수구하기.java
@@ -0,0 +1,8 @@
+package Programmers.SQL;
+
+public class P_특정물고기를잡은총수구하기 {
+//    select COUNT(*) as FISH_COUNT
+//    from FISH_INFO as fi join FISH_NAME_INFO as fni
+//    ON fi.FISH_TYPE = fni.FISH_TYPE
+//    WHERE FISH_NAME in ('BASS', 'SNAPPER');
+}
