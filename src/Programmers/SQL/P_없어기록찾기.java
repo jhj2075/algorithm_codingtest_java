8e58ed0d3caeb82a5e5e7f73a1b837d644807e15
jhj2075 <flg989@naver.com>
2026-05-01 23:54:01 +0900

    05.01

---
+++ src/Programmers/SQL/P_없어기록찾기.java
@@ -0,0 +1,8 @@
+package Programmers.SQL;
+
+public class P_없어기록찾기 {
+//    select animal_id, name
+//    from animal_outs
+//    where animal_id not in (select animal_id from animal_ins)
+//    order by animal_id
+}
